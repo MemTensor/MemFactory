749f671aa105bd3c6e92957246e7fa422dd4e129
Guozl <guozl@memtensor.cn>
2026-03-17 02:41:02 +0000

    fixbugs

--- datas/download.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export HF_ENDPOINT=https://hf-mirror.com
-
-export HF_TOKEN=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  # Read
-
-# huggingface-cli whoami
-
-hf download Qwen/Qwen3-1.7B-Instruct --repo-type model --local-dir /home/models/Qwen3-1.7B-Instruct --token $HF_TOKEN
